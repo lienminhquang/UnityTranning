8ee67e069f45afc02912ee91364a935b2ab16346
lmq <minhquang130698@gmail.com>
2020-10-14 23:49:40 +0700

    Spawn pickup at click position, Move auto player to pickup, Add simple object pool

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,17 @@ public class PlayerController : MonoBehaviour
     public Text m_text;
     public Text m_winText;
 
+    public GameObject m_pickupPool;
+    private PoolManager m_poolManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        m_poolManager = m_pickupPool.GetComponent<PoolManager>();
+
         m_rigidbody = GetComponent<Rigidbody>();
-        m_moveSpeed = 10.0f;
+        m_moveSpeed = 0.2f;
 
         m_score = 0;
         SetScoreText();
@@ -37,23 +42,29 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 move = new  Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        GameObject target = m_poolManager.GetTarget();
+        if(target != null)
+        {
+            Vector3 move = (target.transform.position - transform.position).normalized * m_moveSpeed;
+            //transform.Translate(move);
+            m_rigidbody.MovePosition(transform.position + move * m_moveSpeed);
+        }
 
-        m_rigidbody.AddForce(move * m_moveSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Pickup"))
         {
-            other.gameObject.SetActive(false);
+            m_poolManager.DestroyPickup(other.gameObject);
+
             m_score++;
             SetScoreText();
 
-            if(m_score >= 5)
-            {
-                m_winText.text = "You Win!";
-            }
+            //if(m_score >= 5)
+            //{
+            //    m_winText.text = "You Win!";
+            //}
         }
     }
 }
---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public GameObject m_pickup;
+
+    private Queue<GameObject> m_pickupsPool;
+    private List<GameObject> m_activePickup;
+
+    void Init()
+    {
+        m_activePickup = new List<GameObject>();
+        m_pickupsPool = new Queue<GameObject>(12);
+        for (int i = 0; i < 12; i++)
+        {
+            var free = Instantiate(m_pickup, transform);
+            free.SetActive(false);
+            m_pickupsPool.Enqueue(free);
+        }
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    GameObject Spawn()
+    {
+        if (m_pickupsPool.Count > 0)
+        {
+            var obj = m_pickupsPool.Dequeue();
+            obj.SetActive(true);
+            m_activePickup.Add(obj);
+            return obj;
+        }
+
+        var free = Instantiate(m_pickup, transform);
+        free.SetActive(true);
+        m_activePickup.Add(free);
+        return free;
+
+    }
+
+    public GameObject GetTarget()
+    {
+        if(m_activePickup.Count > 0)
+        {
+            return m_activePickup[0];
+        }
+
+        return null;
+    }
+    public void DestroyPickup(GameObject obj)
+    {
+        obj.SetActive(false);
+        if(m_activePickup.Remove(obj))
+        {
+            m_pickupsPool.Enqueue(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.CompareTag("Ground"))
+                {
+                    Vector3 position = hit.point;
+                    position.y = 0.5f;
+                    GameObject obj = Spawn();
+                    obj.transform.position = position;
+                }
+
+                
+            }
+        }
+    }
+}
