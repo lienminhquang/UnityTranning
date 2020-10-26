45b4077881c95572f0cde711b5e1e2f5b48d2264
lmq <minhquang130698@gmail.com>
2020-10-26 21:35:11 +0700

    + Random color blink

---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int color = Random.Range(0, 3);
+        print(color);
+        GetComponent<Animator>().SetInteger("Color",color);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
